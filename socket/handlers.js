e734512ab306837cf9f071f1ecd8cc566e4693e4
lsheva <lsheva7@gmail.com>
2019-08-20 10:12:15 +0300

    rewrote event generator

--- socket/handlers.js
+++ socket/handlers.js
@@ -1,8 +1,29 @@
-export default (socket) => {
-  socket.on('createRoom', (roomId) => {
+import eventGenerator from "../helpers/event-generator";
+
+export default socket => {
+  console.log("Socket connection established");
+
+  // delete next block after test
+  // const props = {
+  // homeClubId: 2,
+  // awayClubId: 3,
+  // timeout: 10
+  // };
+  // eventGenerator.initGame(props, socket);
+
+  socket.on("createRoom", roomId => {
     socket.join(roomId);
   });
-  socket.on('leaveRoom', (roomId) => {
+  socket.on("leaveRoom", roomId => {
     socket.leave(roomId);
+  });
+  socket.on("simulate", props => {
+    console.log("simulate request");
+    eventGenerator.initGame(props, socket);
+  });
+
+  socket.on("stop-simulation", () => {
+    console.log("stop simulation request");
+    eventGenerator.stopGame();
   });
 };
