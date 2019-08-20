60452c0df057da0a0234c792538dfe9c77419604
lsheva <lsheva7@gmail.com>
2019-08-20 10:23:18 +0300

    added comments

--- config/event-generator.config.js
+++ config/event-generator.config.js
@@ -18,14 +18,29 @@ export const determinedEvents = [
 
 // Event probability is probability of an event during a match
 // For example if probability of a goal is 2, then it is highly
-// expected two goals to be scored
+// expected two goals to be scored during one match. Field without
+// probability considered default event, or just takes all of the
+// rest of probabilities
+
+// Field 'subject' is probabilities of player on particular position
+// to cause an event. It relates to only one event (not whole match),
+// so it has to be normalized to 1 (summ of all probabilities must be
+// equal 1)
+
+// Field after - it is an array of events that could happen after
+// particular event.
+
+// Field direction - "same-player" | "same-team" | "other-team" - who is
+// the subject of next event
+
+// Field inherit -- to be implemented - inherits 'after' properties of other events
 
 export const probableEvents = [
   {
     name: "shot",
     probability: 20,
     subject: {
-      FWD: 0.6,
+      FWD: 0.5,
       MID: 0.3,
       DEF: 0.15,
       GKP: 0.05
