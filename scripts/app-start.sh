090a110ac25813cda4638dce2bbd02dfa016502a
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2019-08-28 18:29:49 +0300

    add deployment config

---
+++ scripts/app-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/bsa-2019-fantasy-faker/
+npm i
+npx sequelize-cli db:migrate
+npx sequelize-cli db:seed:all
+pm2 start --interpreter=./node_modules/.bin/babel-node server.js --name FantasyFaker
---
+++ scripts/before-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu
+pm2 stop FantasyFaker
+sudo rm -rf bsa-2019-fantasy-faker
