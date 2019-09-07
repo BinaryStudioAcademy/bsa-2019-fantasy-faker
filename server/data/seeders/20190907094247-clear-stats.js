736138605169911fd5494e9c6ec41e38242431a2
lsheva <lsheva7@gmail.com>
2019-09-07 12:53:59 +0300

    nulled player stats seed

---
+++ server/data/seeders/20190907094247-clear-stats.js
@@ -0,0 +1,23 @@
+"use strict";
+
+export default {
+  up: (queryInterface, Sequelize) => {
+    const nulledData = {
+      player_score: 0,
+      goals: 0,
+      assists: 0,
+      missed_passes: 0,
+      goals_conceded: 0,
+      saves: 0,
+      yellow_cards: 0,
+      red_cards: 0,
+      transfers_in: 0,
+      transfers_out: 0
+    };
+    queryInterface.bulkUpdate("player_stats", nulledData);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("player_stats", null, {});
+  }
+};
